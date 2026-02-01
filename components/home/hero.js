8cc7448f0b4134c04171723ccc0beaac9798919f
mac <mac@macs-MacBook-Air.local>
2026-02-01 15:03:33 +0530

    A

--- components/home/hero.js
+++ components/home/hero.js
@@ -87,7 +87,7 @@ export default function Hero({ locale, CTALocale }) {
 					<Image
 						width={1024}
 						height={600}
-						src={'/bg-white.PNG'}
+						src={'/bg-white.png'}
 						className='hidden md:flex w-full -mt-10'
 						alt='app demo'
 					/>
