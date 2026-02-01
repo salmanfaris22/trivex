322863f001da5d218f679c3da82506f90c6ea9f8
mac <mac@macs-MacBook-Air.local>
2026-02-01 15:34:42 +0530

    fix image domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-			  protocol: 'https',
-        hostname: 'landingpage.huglemon.com',
-        pathname: '/**', // ✅ IMPORTANT
-			},
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "landingpage.huglemon.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "huglemon.com",   // add this
+        pathname: "/**",
+      },
+    ],
+  },
 };
+
+
+
 
 export default nextConfig;
